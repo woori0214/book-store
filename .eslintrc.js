78a0617e13db8063378fb687e0e827876b3f7c6e
Minto <xpeed96@gmail.com>
2023-02-15 20:46:50 +0900

    feat: eslintrc 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 0,
+    'comma-dangle': ['error', 'never'],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor']
+      }
+    ],
+    'react/prop-types': 'off',
+    'object-curly-newline': ['error', { ObjectExpression: 'always', ObjectPattern: { multiline: true }, ImportDeclaration: 'never', ExportDeclaration: { multiline: true, minProperties: 3 } }],
+    'max-len': ['error', { code: 300 }]
+  }
+};
