62cac1818aef9c91162388ee00d3a3c792615531
Kwak-YuJin <dbwls6285@naver.com>
2023-02-16 17:30:53 +0900

    feat: 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './Components/commons/Nav'
+import Router from './routes';
+import Footer from './Components/commons/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav/>
+    <Router/>
+    <Footer/>
+    </>
   );
 }
 
