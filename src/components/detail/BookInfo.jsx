0e9d12f367a8ff76745f59008f210d22cfc9a9f6
yujin-kwak <yujin02371@gmail.com>
2023-02-21 14:28:13 +0900

    refactor: 사용 안하는 css 삭제~

--- src/components/detail/BookInfo.jsx
+++ src/components/detail/BookInfo.jsx
@@ -46,49 +46,6 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
-const BookInfoWrapper = styled.div`
-  display: flex;
-  position: relative;
-`;
-
-const FoundBookImg = styled.img`
-  width: 326px;
-  height: 462px;
-  margin-top: 85px;
-`;
-
-const DescriptionTable = styled.table`
-  box-sizing: border-box;
-  width: 761px;
-  height: 414px;
-  border-top: 4px solid #353535;
-  margin: 100px 0 0 94px;
-  border-collapse: collapse;
-`;
-
-const DescriptionTr = styled.tr`
-  height: 99px;
-  &:first-child {
-    height: 117px;
-  }
-`;
-
-const DescriptionTd = styled.td`
-  vertical-align: middle;
-  &:first-child {
-    width: 178px;
-    text-align: center;
-    font-family: ${props =>
-      props.bold ? 'NotoSansKR-Bold' : 'NotoSansKR-Regular'};
-    font-size: 30px;
-    line-height: ${props => (props.bold ? '41px' : '43px')};
-  }
-  margin-left: 28px;
-  border-bottom: 1px solid #b5b5b5;
-  font-size: 20px;
-  line-height: 29px;
-`;
-
 const ButtonWrapper = styled.div`
   display: flex;
   position: absolute;
