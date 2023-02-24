7b450e11a795dad0d8a44267410e03b36b65e154
Minto <xpeed96@gmail.com>
2023-02-24 09:59:36 +0900

    style: shoppingCart 컴포넌트 수정

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -35,18 +35,14 @@ const data = [
 
 localStorage.setItem('books', JSON.stringify(data));
 
+const getInitailValue = () => {
+  const basketItems = localStorage.getItem('books');
+  return basketItems ? JSON.parse(basketItems) : [];
+};
+
 function ShoppingCartPage() {
   // 장바구니에 추가한 책들의 데이터
-  const [books, setBooks] = useState(JSON.parse(localStorage.getItem('books')));
-
-  if (!books) {
-    return (
-      <>
-        <CartTitle>장바구니</CartTitle>
-        <EmptyCart>장바구니에 물건을 추가해주세요.</EmptyCart>
-      </>
-    );
-  }
+  const [books, setBooks] = useState(getInitailValue());
 
   const totalAmount = useMemo(
     () => books.reduce((sum, curr) => sum + curr.quantity * curr.price, 0),
@@ -106,28 +102,32 @@ function ShoppingCartPage() {
 
   return (
     <>
-      <CartTitle>장바구니</CartTitle>
-      <CartWrapper>
-        <CartContent>
-          <CartHeader>
-            <Button type="button" onClick={handleDeleteAll}>
-              전체삭제
-            </Button>
-          </CartHeader>
-          <CartList>
-            {books.map(book => (
-              <CartItem
-                key={book.id}
-                book={book}
-                onDelete={handleDelete}
-                onMinus={handleMinus}
-                onPlus={handlePlus}
-              />
-            ))}
-          </CartList>
-        </CartContent>
-        <CartAcount totalAmount={totalAmount} />
-      </CartWrapper>
+      <PageTitle title="장바구니" />
+      {books.length === 0 ? (
+        <EmptyCart>장바구니에 물건을 추가해주세요.</EmptyCart>
+      ) : (
+        <CartWrapper>
+          <CartContent>
+            <CartHeader>
+              <Button type="button" onClick={handleDeleteAll}>
+                전체삭제
+              </Button>
+            </CartHeader>
+            <CartList>
+              {books.map(book => (
+                <CartItem
+                  key={book.id}
+                  book={book}
+                  onDelete={handleDelete}
+                  onMinus={handleMinus}
+                  onPlus={handlePlus}
+                />
+              ))}
+            </CartList>
+          </CartContent>
+          <CartAcount totalAmount={totalAmount} />
+        </CartWrapper>
+      )}
     </>
   );
 }
