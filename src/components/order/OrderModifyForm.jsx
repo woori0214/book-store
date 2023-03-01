5c6fad487c35fa18272ecded0722137781f56e70
yujin-kwak <yujin02371@gmail.com>
2023-03-01 13:10:22 +0900

    style: 주문 수정 구조 변경

--- src/components/order/OrderModifyForm.jsx
+++ src/components/order/OrderModifyForm.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import CommonButton from 'components/commons/button/Button';
 import OrderTemplate from './OrderTemplate';
 import Api from 'utils/api';
 import OrderFormTemplate from './OrderFormTemplate';
+import PageTitle from 'components/commons/pageTitle/PageTitle';
 
-function OrderModifyForm({ initialOrdererInfo, orderId }) {
+function OrderModifyForm() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const initialOrderInfo = location.state.initialOrdererInfo;
+  const initialOrdererInfo = initialOrderInfo.order;
+  const orderId = initialOrderInfo.orderItemList[0].orderID;
+
   const [ordererInfo, setOrdererInfo] = useState({
     ordererName: initialOrdererInfo.userName,
     ordererEmail: initialOrdererInfo.email,
     ordererPhone: initialOrdererInfo.phone,
     ordererAddress: initialOrdererInfo.address
   });
-  const navigate = useNavigate();
+
+  console.log('initialOrdererInfo', initialOrdererInfo);
+  console.log('orderId', orderId);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -66,21 +75,24 @@ function OrderModifyForm({ initialOrdererInfo, orderId }) {
   };
 
   return (
-    <Wrapper>
-      <OrderTemplate templateTitle="주문정보" />
-      <OrderFormTemplate handleChange={handleChange} ordererInfo={ordererInfo} />
-      <CommonButton
-        buttonTitle="수정 완료"
-        borderColor="#9E8CEC"
-        width="120px"
-        height="50px"
-        fontSize="1.3rem"
-        lineHeight="2.5rem"
-        borderRadius="15px"
-        margin="2.5rem auto 0"
-        onClick={handleModifyComplete}
-      />
-    </Wrapper>
+    <>
+      <PageTitle title="주문 정보 수정" />
+      <Wrapper>
+        <OrderTemplate templateTitle="주문정보" />
+        <OrderFormTemplate handleChange={handleChange} ordererInfo={ordererInfo} />
+        <CommonButton
+          buttonTitle="수정 완료"
+          borderColor="#9E8CEC"
+          width="120px"
+          height="50px"
+          fontSize="1.3rem"
+          lineHeight="2.5rem"
+          borderRadius="15px"
+          margin="2.5rem auto 0"
+          onClick={handleModifyComplete}
+        />
+      </Wrapper>
+    </>
   );
 }
 
@@ -89,7 +101,7 @@ const Wrapper = styled.div`
   width: 90%;
   min-width: 453px;
   max-width: 918px;
-  margin: 2rem auto 0;
+  margin: 3rem auto 0;
   > button {
     position: absolute;
     right: 0;
--- src/pages/order/OrderModifyPage.jsx
+++ src/pages/order/OrderModifyPage.jsx
@@ -1,29 +1,7 @@
-import styled from 'styled-components';
-import PageTitle from 'components/commons/pageTitle/PageTitle';
-import { useLocation } from 'react-router-dom';
 import OrderModifyForm from 'components/order/OrderModifyForm';
 
 function OrderModifyPage() {
-  const location = useLocation();
-  const initialOrderInfo = location.state.initialOrdererInfo;
-
-  const initialOrdererInfo = initialOrderInfo.order;
-  const orderId = initialOrderInfo.orderItemList[0].orderID;
-
-  console.log('initialOrdererInfo', initialOrdererInfo);
-  console.log('orderId', orderId);
-
-  return (
-    <>
-      <PageTitle title="주문 정보 수정" />
-      <FormWrapper>
-        <OrderModifyForm initialOrdererInfo={initialOrdererInfo} orderId={orderId} />
-      </FormWrapper>
-    </>
-  );
+  return <OrderModifyForm />;
 }
 
-const FormWrapper = styled.div`
-  margin-top: 66px;
-`;
 export default OrderModifyPage;
